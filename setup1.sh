909a48c891fc2ec5b0467b599b604c77ca7c75e4
Faraaz Khan <75445343+faraazkhan1112@users.noreply.github.com>
2022-12-05 10:42:28 +0530

    Add files via upload

---
+++ setup1.sh
@@ -0,0 +1,37 @@
+# Fail the script if one command fails
+set -e
+
+pip install -r requirements.txt
+
+if [[ ! -d 'checkpoints' ]]
+then
+  wget -nc https://github.com/microsoft/Bringing-Old-Photos-Back-to-Life/releases/download/v1.0/global_checkpoints.zip
+  unzip global_checkpoints.zip
+fi
+
+if [[ ! -d 'weights' ]]
+then
+  mkdir 'weights'
+
+  while read -r line; do
+    echo "Downloading $line"
+    wget -nc "$line" -P weights
+  done < gpen_urls.txt
+
+  for i in $(find 'weights' -type f)
+  do
+      mv "$i" "$(echo "$i" | cut -d? -f1)"
+  done
+fi
+
+if [[ ! -d 'models' ]]
+then
+#  wget -nc https://data.deepai.org/deoldify/ColorizeArtistic_gen.pth -P models
+  mkdir models
+  wget -nc https://www.dropbox.com/s/usf7uifrctqw9rl/ColorizeStable_gen.pth?dl=0 -P models
+  mv ./models/ColorizeStable_gen.pth?dl=0 ./models/ColorizeStable_gen.pth
+  for i in $(find 'weights' -type f)
+  do
+      mv "$i" "$(echo "$i" | cut -d? -f1)"
+  done
+fi
